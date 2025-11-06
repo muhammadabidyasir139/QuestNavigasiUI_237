3522e633ae8bbb3d66e2bc00533f00a5f0a0ae06
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-11-06 16:49:08 +0700

    membuat kolom nama dan menambahkan namanya

--- app/src/main/java/com/example/navigationapp/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/navigationapp/view/DaftarPeserta.kt
@@ -27,5 +27,26 @@ fun DaftarPeserta(
             modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
+        ) {
+            // NAMA LENGKAP
+            Text(
+                text = stringResource(id = R.string.label_nama_lengkap),
+                fontSize = 14.sp,
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 20.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.nama_lengkap_contoh),
+                fontSize = 18.sp,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
+
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Gray
+            )
     }
