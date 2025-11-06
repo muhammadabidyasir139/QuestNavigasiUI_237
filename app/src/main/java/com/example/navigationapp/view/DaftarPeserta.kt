547ec80f5c38ad53c5278b5584c5799325663f07
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-11-06 16:46:51 +0700

    membuat scaffold pada halaman

--- app/src/main/java/com/example/navigationapp/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/navigationapp/view/DaftarPeserta.kt
@@ -2,6 +2,22 @@ package com.example.navigationapp.view
 
 @Composable
 fun DaftarPeserta(
+    modifier : Modifier = Modifier,
     onBackToHome: () -> Unit,
     onGoToForm: () -> Unit
-)
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.title_list_peserta),
+                        color = Color.White,
+                        fontSize = 20.sp
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+}
