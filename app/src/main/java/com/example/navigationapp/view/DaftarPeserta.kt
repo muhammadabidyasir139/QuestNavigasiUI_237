3fc16f0e24cabaa38d842afce0b41e1212ae9b33
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-11-06 16:52:53 +0700

    menambahkan string untuk beranda

--- app/src/main/java/com/example/navigationapp/view/DaftarPeserta.kt
+++ app/src/main/java/com/example/navigationapp/view/DaftarPeserta.kt
@@ -1,5 +1,17 @@
 package com.example.navigationapp.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationapp.R
+
 @Composable
 fun DaftarPeserta(
     modifier : Modifier = Modifier,
